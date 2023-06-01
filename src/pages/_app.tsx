19791449494f43230e3806f25a42bdcf0f58f845
gracebir <grace.devolop@gmail.com>
2023-06-01 11:16:09 +0200

    getstarted

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Quicksand } from 'next/font/google'
+
+const quicksand = Quicksand({ subsets: ['latin'] })
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={quicksand.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-semibold text-2xl">hello me</h1>
+    </div>
+  )
+}
