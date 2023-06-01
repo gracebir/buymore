36409fb8a1feaabda241715a5187eea5cca2c757
gracebir <grace.devolop@gmail.com>
2023-06-01 19:50:05 +0200

    adding head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,6 @@ export default function Home() {
   return (
     <div className="">
       <Layout>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-
-        <h1 className="text-base py-4">E-commerce</h1><h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        <h1 className="text-base py-4">E-commerce</h1>
-        
       </Layout>
     </div>
   )
