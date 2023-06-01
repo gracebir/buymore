58049e9f370302cb7c08e44629823e370a008cd8
gracebir <grace.devolop@gmail.com>
2023-06-01 15:05:04 +0200

    adding navbar to the layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 type layoutProp = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-function Layout({children}:layoutProp) {
+function Layout({ children }: layoutProp) {
   return (
-    <div className="mx-auto max-w-6xl px-4 lg:px-0">
-      {children}
+    <div>
+      <Navbar/>
+      <div className="mx-auto max-w-6xl px-4 lg:px-0">
+        {children}
+      </div>
     </div>
   )
 }
