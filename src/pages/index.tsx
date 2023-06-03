96fb6ce266bf0f850a91668c7758a563d5d0dabf
gracebir <grace.devolop@gmail.com>
2023-06-03 16:57:29 +0200

    product section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Layout from "@/components/Layout";
+import ProductSection from "@/components/ProductSection";
 
 export default function Home() {
   return (
     <div>
       <Layout>
         <Hero/>
+        <ProductSection/>
       </Layout>
     </div>
   )
