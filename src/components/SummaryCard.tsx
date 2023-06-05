cf6a97f9708e2f168f3b1bbf21b48983a0665deb
gracebir <grace.devolop@gmail.com>
2023-06-05 17:02:39 +0200

    Summary price card

---
+++ src/components/SummaryCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function SummaryCard() {
+  return (
+    <div className="w-full lg:w-[350px] border rounded-md border-gray-700 flex flex-col gap-4 pb-3">
+      <div className='px-4 flex justify-between py-3 border-b border-gray-700'>
+        <h3 className='font-bold'>Summary</h3>
+      </div>
+      <div className='px-4 flex justify-between'>
+        <h3>Sub Total</h3>
+        <span>$0</span>
+      </div>
+      <div className='px-4 flex justify-between'>
+        <h3>Shipping Fees</h3>
+        <span>$0</span>
+      </div>
+      <div className='px-4 flex border-t pt-2 border-gray-700 flex-col gap-4'>
+        <div className='flex justify-between'>
+          <h3 className='font-bold'>Total</h3>
+          <span>$0</span>
+        </div>
+        <button className='bg-blue-color py-2 rounded-md'>Checkout</button>
+      </div>
+    </div>
+  )
+}
+
+export default SummaryCard
