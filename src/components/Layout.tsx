fec24601e1091d9ad875ee4a636a4a896cb6cf90
gracebir <grace.devolop@gmail.com>
2023-06-06 10:31:23 +0200

    add a min height to base layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ function Layout({ children }: layoutProp) {
   return (
     <div>
       <Navbar/>
-      <div className="mx-auto max-w-6xl min-h-[75vh] px-4 lg:px-0 mt-[84px] lg:mt-0">
+      <div className="mx-auto max-w-6xl min-h-[72vh] px-4 lg:px-0 mt-[84px] lg:mt-0">
         {children}
       </div>
       <Footer/>
