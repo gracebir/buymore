c527030ea987b540b6973913e3d2ea072b83c6ca
gracebir <grace.devolop@gmail.com>
2023-06-06 15:50:40 +0200

    sign up page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,10 +1,16 @@
+import Layout from '@/components/Layout'
+import SignupForm from '@/components/SignupForm'
 import React from 'react'
 
 function SignUp() {
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <div className='py-8'>
+        <div className='border border-dark-color max-w-3xl grid lg:grid-cols-2 sm:grid-cols-1 p-4 mx-auto'>
+          <SignupForm/>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
