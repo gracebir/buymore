acf454d6f52350b4ba2eaafe3a491a42f4b5cb9f
gracebir <grace.devolop@gmail.com>
2023-06-08 21:37:41 +0200

    add signin validation schema

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -3,7 +3,11 @@ import * as yup from 'yup';
 export const signUpSchema = yup.object().shape({
     name: yup.string().required("Required"),
     email: yup.string().email().required("Required"),
-    phone: yup.string().min(10),
     password: yup.string().min(6).required("Required"),
     confirmPassword: yup.string().oneOf([yup.ref("password")]).required("Required")
+})
+
+export const signInSchema = yup.object().shape({
+    email: yup.string().email().required("Required"),
+    password: yup.string().min(8).required("Required"),
 })
